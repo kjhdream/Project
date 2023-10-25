b0cd815992c90e1686292feaa515e7c5b45a33d6
kjhdream <137378938+kjhdream@users.noreply.github.com>
2023-10-25 14:19:39 +0900

    Delete Terraform/gar.tf

--- Terraform/gar.tf
+++
@@ -1,6 +0,0 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/artifact_registry_repository
-resource "google_artifact_registry_repository" "gcpzg_gar" {
-  repository_id = "gcpzg-gar" # Required | Name of gar
-  format        = "DOCKER" # Required | The format of packages
-  location      = "asia-northeast3"
-}
