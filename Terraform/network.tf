2d6fcec9040cf77bc77c4b88b6a6a4dcc2380e99
kjhdream <137378938+kjhdream@users.noreply.github.com>
2023-10-27 18:27:47 +0900

    Update network.tf

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -1,42 +1,21 @@
-resource "google_compute_network" "vpc" {
-  name                    = "vpc"
+resource "google_compute_network" "gcpzg_vpc" {
+  name                    = "gcpzg-vpc"
   auto_create_subnetworks = false
+
+  # mtu(최대 전송 단위) 설정 옵션(default = 1460) 최댓값 = 1500
+  # mtu = 1460 #default: 1460
 }
 
-resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet"
-  ip_cidr_range = "10.0.0.0/24"
-  network       = google_compute_network.vpc.id
-
-  private_ip_google_access = true
-
-  log_config {
-    aggregation_interval = "INTERVAL_30_SEC"
-    flow_sampling = "0.5"
-  }
-}
-
-resource "google_compute_subnetwork" "subnet_proxy" {
-  name          = "subnet-proxy"
+resource "google_compute_subnetwork" "gcpzg_subnet_ane3" {
+  name          = "gcpzg-subnet-ane3"
   ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.vpc.id
+  network       = google_compute_network.gcpzg_vpc.id
 
+  # 비공개 구글 액세스 사용 설정(DB 등 액세스 용도)
   private_ip_google_access = true
 
   log_config {
     aggregation_interval = "INTERVAL_30_SEC"
     flow_sampling = "0.5"
   }
-}
-
-resource "google_service_networking_connection" "db_connection_prod" {
-  network                 = google_compute_network.vpc.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.ip_db_address_prod.name]
-}
-
-resource "google_service_networking_connection" "db_connection_test" {
-  network                 = google_compute_network.vpc.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.ip_db_address_test.name]
 }
