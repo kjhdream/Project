e4f2c51ef6b75ec04cc7cc960fdd3efae8b72142
kjhdream <137378938+kjhdream@users.noreply.github.com>
2023-10-27 19:46:13 +0900

    Update ip-address.tf

--- Terraform/ip-address.tf
+++ Terraform/ip-address.tf
@@ -1,5 +1,21 @@
-resource "google_compute_global_address" "ingress_ip" {
-  name = "ingress-static-ip"
-  // INTERNAL, EXTERNAL 두 종류가 있음
- # address_type = "EXTERNAL"  // Default : EXTERNAL
+resource "google_compute_global_address" "ip_ingress_address" {
+  name = "ip-ingress-address"
+}
+
+resource "google_compute_global_address" "ip_db_address_prod" {
+  name          = "ip-db-address-prod"
+
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_global_address" "ip_db_address_test" {
+  name          = "ip-db-address-test"
+
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = google_compute_network.vpc.id
 }
